75d10e52f5b4b73381684ffb82ff75dd943d18d9
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-12-19 19:38:51 +0100

    feat: add exercise 1 and 2

--- README.md
+++ README.md
@@ -12,7 +12,27 @@ npm install
 
 ```bash
 node script.js
+node exercise.js
 ```
+
+## Exercise
+Tu vas envoyer plusieurs transactions comme celles qu’on a déjà faites, pour transférer de l’ether.
+
+Pour ces transactions tu vas à chaque fois spécifier manuellement tous les champs dont on a parlé : nonce, type, gas, gasLimit.
+
+Tu vas remplir chacun des critères suivants :
+
+- Envoyer une transaction de type 0
+- Envoyer une transaction de type 2
+- Envoyer une transaction avec un nonce trop petit
+- Envoyer une transaction avec un nonce trop grand
+- Envoyer une transaction en diminuant le prix pour chaque gas
+- Envoyer une transaction en augmentant le prix pour chaque gas
+- Envoyer une transaction avec un gasLimit trop bas
+
+Utilise le hash de ces transactions pour aller les voir sur Etherscan. Tu verras que tu pourras retrouver tous ces champs sur la page de la transaction (clique bien sur “+ Click to show more” en bas de la page)
+
+Envoie-moi les liens Etherscan vers les transactions pour chacun de ces critères.
 
 ## License
 
--- exercise.js
+++ exercise.js
@@ -10,5 +10,72 @@ const privateKey = process.env.PRIVATE_KEY;
 const signer = new ethers.Wallet(privateKey, provider);
 const fromAddress = process.env.PUBLIC_KEY;
 const toAddress = process.env.PUBLIC_KEY_TO;
+const weiToSend = 3n;
 
+const sendType0 = async () => {
+    const nonce = await signer.getNonce();
+    const type = 0;
+    const feeData = await provider.getFeeData();
+    
+    const gasEtimate = await provider.estimateGas({
+        to: toAddress,
+        value: weiToSend,
+        type: type,
+        gasPrice: feeData.gasPrice,
+    });
 
+    const tx = await signer.sendTransaction({
+        to: toAddress,
+        value: weiToSend,
+        nonce: nonce,
+        type: type,
+        gasPrice: feeData.gasPrice,
+        gasLimit: gasEtimate,
+    });
+    console.log(`Tx type 0 sent: ${tx.hash}`, {
+        to: toAddress,
+        value: weiToSend,
+        nonce: nonce,
+        type: type,
+        gasPrice: feeData.gasPrice,
+        gasLimit: gasEtimate,
+    });
+}
+
+const sendType2 = async () => {
+    const nonce = await signer.getNonce();
+    const type = 2;
+    const feeData = await provider.getFeeData();
+    console.log("Fee Data:", feeData);
+
+    const gasEstimate = await provider.estimateGas({
+        to: toAddress,
+        value: weiToSend,
+        type: type,
+        maxFeePerGas: feeData.maxFeePerGas,
+        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas
+    });
+
+    const tx = await signer.sendTransaction({
+        to: toAddress,
+        value: weiToSend,
+        nonce: nonce,
+        type: type,
+        maxFeePerGas: feeData.maxFeePerGas,
+        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+        gasLimit: gasEstimate,
+    });
+
+    console.log(`Tx type 2 sent: ${tx.hash}`, {
+        to: toAddress,
+        value: weiToSend,
+        nonce: nonce,
+        type: type,
+        maxFeePerGas: feeData.maxFeePerGas,
+        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+        gasLimit: gasEstimate,
+    });
+}
+
+// await sendType0(); // 0xd6c8c0c00215abbe4d754046e78e9f08b85fcfbf9e016c911f5b6fa4772c7d69
+// await sendType2(); // 0x6bfcfc55f10cad188c9d3dc910aa084d2f501908f5b2430bd3b980ceb63d6f7d
